6fb8709c7b3958d61292b92edf626f5f58264548
Jhymer Martinez <jamartinezg@unl.edu.ec>
2013-11-13 02:13:31 -0500

    mejorado el scanner

--- Analizador_Lexico_TC/main.cpp
+++ Analizador_Lexico_TC/main.cpp
@@ -20,22 +20,28 @@ void obtener_matriz(string matriz[50][50], int rango[3]) {
     int count = 0;
 
     ifstream archivo_tabla("tabla.csv");
-
+    //lectura del csv
     while (!archivo_tabla.eof()) {
-
+        //para leer linea por linea se usa un archivo auxiliar
+        //donde se escribe la linea leida del csv
+        //y se la lle nuevamente para ser almacenada en una fila de
+        // la matriz
         archivo_tabla.getline(cadena, 128, '\n');
         ofstream archivo_aux("auxtt.txt");
         archivo_aux << cadena << endl;
         archivo_aux.close();
-
         ifstream archivo_aux_lec("auxtt.txt");
-
         while (!archivo_aux_lec.eof()) {
+            //hasta encontrar el ultimo ;
             archivo_aux_lec.getline(cadenaAUX, 128, ';');
             string cadenaSTR(cadenaAUX);
+            //se va llenando la matriz con cada linea
             matriz[fila][column] = cadenaSTR;
             column++;
         }
+        //almaceno la columna reducida en 1 para posteriores usos
+        //se utiliza el if para almacenar el valor de column antes de que count 
+        //incremente
         if (count == 0) {
             y = column - 1;
         }
@@ -46,76 +52,101 @@ void obtener_matriz(string matriz[50][50], int rango[3]) {
         archivo_aux_lec.close();
     }
     archivo_tabla.close();
+    //se guardan los valores para los posteriores recorridos
     rango[0] = x;
     rango[1] = y;
 
 }
 
+//genera ambas tablas compactas a partir de la matriz inicial
 void obtener_tablas(string matriz[50][50], string matriz_tabla1[50][50], string matriz_tabla2[50][50], int rango[3]) {
     string valoresC3[50];
     int cont = 0;
-
+    ////////////// TABLA 1 ///////////////////
+    //se completa c1 con un contador desde 1
     for (int i = 1; i < rango[0]; i++) {
-        stringstream stream;
-        stream << i;
-        matriz_tabla1[i - 1][0] = stream.str();
+        //conversion entero->string
+        stringstream stream_para_c1;
+        stream_para_c1 << i;
+        matriz_tabla1[i - 1][0] = stream_para_c1.str();
     }
-
+    
+    //recorrido de la matriz para conseguir el numero de valores
+    //diferentes de cero
     for (int i = 1; i < rango[0]; i++) {
         for (int j = 1; j < rango[1]; j++) {
             if (matriz[i][j] != "0") {
                 cont++;
             }
         }
-        stringstream stream;
-        stream << cont;
-        valoresC3[i - 1] = stream.str();
+        //conversion int a string
+        stringstream stream_para_c3;
+        stream_para_c3 << cont;
+        valoresC3[i - 1] = stream_para_c3.str();
+        //se regresa cont a cero y se continua con la 
+        //siguiente fila
         cont = 0;
     }
-
+    //se completa el resto de la tabla
     for (int i = 0; i < rango[0] - 1; i++) {
+        //se pasan los valores almacenados anteriormente a C3
         matriz_tabla1[i][2] = valoresC3[i];
+        //se pone en 1 el primer valor de C2
+        //solo si es la primera vuelta
         if (i == 0) {
             matriz_tabla1[i][1] = "1";
         } else {
+            //caso contrario se continua con el proceso normal
             int aux1 = atoi(matriz_tabla1[i - 1][1].c_str());
             int aux2 = atoi(matriz_tabla1[i - 1][2].c_str());
             int suma = aux1 + aux2;
-            stringstream stream;
-            stream << suma;
-            matriz_tabla1[i][1] = stream.str();
+            stringstream stream_para_c2;
+            stream_para_c2 << suma;
+            matriz_tabla1[i][1] = stream_para_c2.str();
         }
     }
+    
+    //////////////// TABLA 2 ///////////////////
     int cont2 = 0;
     for (int i = 1; i < rango[0]; i++) {
         for (int j = 1; j < rango[1]; j++) {
+            //si el elemento es distinto de cero 
+            //entonces lo almacena
             if (matriz[i][j] != "0") {
-                stringstream stream;
-                stringstream stream2;
-                stream << cont2 + 1;
-                matriz_tabla2[cont2][0] = stream.str();
+                stringstream stream_para_d1;
+                stringstream stream_para_d3;
+                //conversion entero -> string
+                stream_para_d1 << cont2 + 1;
+                matriz_tabla2[cont2][0] = stream_para_d1.str();
+                //valores de la matriz
                 matriz_tabla2[cont2][1] = matriz[i][j];
-                stream2 << j;
-                matriz_tabla2[cont2][2] = stream2.str();
+                //conversion entero -> string
+                stream_para_d3 << j;
+                matriz_tabla2[cont2][2] = stream_para_d3.str();
                 cont2++;
             }
         }
     }
+    //se almacena el valor para posteriores recorridos
     rango[2] = cont2;
 }
 
 string leer_palabra() {
     string palabra;
-    string palabra2;
-
+    string palabra_retorno;
+    //obtiene la frase ingresada por teclado
     cin.getline((char*) palabra.c_str(), 256);
-    palabra2 = palabra.c_str();
-    return palabra2.c_str();
+    //retorna el string
+    palabra_retorno = palabra.c_str();
+    return palabra_retorno.c_str();
 }
 
+//este metodo recibe una letra y mediante recorrido por el alfabeto
+//que tambien se encuentra en la matriz principal,
+//devuelve su ubicacion segun la columna donde se encuentre
 int retornar_posicion(string letra, string matriz[50][50], int rango[3]) {
-
     for (int j = 1; j < rango[1]; j++) {
+        //solo recorre la fila 0 que es donde esta el alfabeto
         if (letra == matriz[0][j]) {
             return j;
             break;
@@ -123,23 +154,26 @@ int retornar_posicion(string letra, string matriz[50][50], int rango[3]) {
     }
 }
 
+//metodo que permite interacccion entre las dos tablas, teniendo como fin el retorno de un valor que se utilizara 
+//en el proximo ciclo 
 int recuperar_valor(int valor, int verificador, int retorno, string matriz_tabla1[50][50], string matriz_tabla2[50][50], int rango[3]) {
-
-
+    
+    //verificador indica si es la primera vuelta o si ya se han ejecutado otras anteriores
+    //cero indica q es la primera
     if (verificador == 0) {
-        //int c1 = atoi(matriz_tabla1[0][0].c_str());
-        //int c2 = atoi(matriz_tabla1[0][1].c_str());
+        //se define el numero de intentos para encontrar el elemento
+        //buscado
         int c3 = atoi(matriz_tabla1[0][2].c_str());
-        //int d1 = atoi(matriz_tabla2[0][0].c_str());
-        //int d2 = atoi(matriz_tabla2[0][1].c_str());
-        //int d3 = atoi(matriz_tabla2[0][2].c_str());
         for (int i = 0; i < c3; i++) {
+            //valor es el numero del elemento encontrado en la matriz
             if (valor == atoi(matriz_tabla2[i][2].c_str())) {
+                //se retorna un entero
                 return atoi(matriz_tabla2[i][1].c_str());
                 break;
             }
         }
     } else {
+        //procedimiento similar con el resto de elementos de la palabra
         int c1 = retorno;
         int c2;
         int c3;
@@ -151,39 +185,43 @@ int recuperar_valor(int valor, int verificador, int retorno, string matriz_tabla
             }
         }
         int aux = c2;
-
         for (int i = 0; i < c3; i++) {
             if (valor == atoi(matriz_tabla2[aux - 1][2].c_str())) {
                 return atoi(matriz_tabla2[aux - 1][1].c_str());
             }
             aux++;
         }
-
     }
 }
 
 void algoritmo_validar(string matriz[50][50], string matriz_tabla1[50][50], string matriz_tabla2[50][50], int rango[3], string palabra) {
 
     string valores_palabra[50];
-    int val;
+    int posicion;
     int contFinal = 1;
     int resultados[30];
+    //En caso de q no se haya ingresado nada
     if (palabra == "" || palabra == " " || palabra == "\t") {
-        cout << "No se ha ingresado ninguna palabra" << endl;
+        cout << "Error: No se ha ingresado ninguna palabra\n" << endl;
         exit(0);
     }
     for (int i = 0; i < palabra.size(); i++) {
-        stringstream stream;
-        stream << palabra[i];
-        val = retornar_posicion(stream.str(), matriz, rango);
-
-        if (val != 0) {
-            stringstream valStr;
-            valStr << val;
-            valores_palabra[i] = valStr.str();
+        //conversion char -> string
+        stringstream stream_caracter;
+        stream_caracter << palabra[i];
+        posicion = retornar_posicion(stream_caracter.str(), matriz, rango);
+        //si se retorna una posicion distinta a cero
+        if (posicion != 0) {
+            //conversion int -> string
+            stringstream stream_posicion;
+            stream_posicion << posicion;
+            valores_palabra[i] = stream_posicion.str();
+            //este contador se usara para luego recorrer el arreglo
             contFinal++;
         } else {
-            cout << "Sintaxis incorrecta" << endl;
+            //en caso de retornar cero muestra mensaje de error y
+            //termina el programa
+            cout << "Error: Sintaxis incorrecta\n" << endl;
             exit(0);
             break;
         }
@@ -220,7 +258,7 @@ void algoritmo_validar(string matriz[50][50], string matriz_tabla1[50][50], stri
         //entonces se presenta mensaje y finaliza el programa
          for (int i = 0; i < cont; i++) {
             if (resultados[i] == 0) {
-                cout << "Sintaxis incorrecta" << endl;
+                cout << "Error: Sintaxis incorrecta\n" << endl;
                 exit(0);
             }
         }
@@ -233,7 +271,7 @@ void algoritmo_validar(string matriz[50][50], string matriz_tabla1[50][50], stri
             }
         }
     } else {
-        cout << "Sintaxis incorrecta" << endl;
+        cout << "Error: Sintaxis incorrecta\n" << endl;
         exit(0);
     }
 
@@ -278,13 +316,15 @@ int main() {
     palabra = leer_palabra();
     
     //en caso de q no se ingrese un espacio o tabulador al final
-    char aux = palabra[palabra.size()-1];
-    stringstream s1;
-    s1 << aux;
-    if (s1.str() != " " && s1.str() != "        ") {
-        stringstream ss;
-        ss << palabra.c_str() << " ";
-        palabra = ss.str();
+    char palabra_aux = palabra[palabra.size()-1];
+    //conversion char -> string
+    stringstream stream_palabra_aux;
+    stream_palabra_aux << palabra_aux;
+    if (stream_palabra_aux.str() != " " && stream_palabra_aux.str() != "        ") {
+        //concatenacion agregando un espacio en blanco
+        stringstream stream_concat;
+        stream_concat << palabra.c_str() << " ";
+        palabra = stream_concat.str();
         algoritmo_validar(matriz, matriz_tabla1, matriz_tabla2, rango, palabra);
     } else {
         algoritmo_validar(matriz, matriz_tabla1, matriz_tabla2, rango, palabra);
