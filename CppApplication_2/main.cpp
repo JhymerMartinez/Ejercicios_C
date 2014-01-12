b5dc92f73c4cc1b9fb1b34193ffc7bea6631de65
Jhymer Martínez <jhmartinez1991@gmail.com>
2014-01-12 17:23:34 -0500

    mejorando AL

---
+++ CppApplication_2/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: Piedra
+ *
+ * Created on 11 de enero de 2014, 06:33 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
---
+++ app_prueba/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: Piedra
+ *
+ * Created on 11 de enero de 2014, 05:44 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"hola mundo"<<endl;
+    return 0;
+}
+
